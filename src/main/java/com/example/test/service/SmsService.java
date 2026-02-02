15b5fd520d996fd09732c218582e22ddf160fa5a
test <tiuy11546@gmail.com>
2026-02-03 00:03:22 +0530

    page added

--- src/main/java/com/example/test/service/SmsService.java
+++ src/main/java/com/example/test/service/SmsService.java
@@ -5,6 +5,7 @@ import com.example.test.entities.Sms;
 import com.example.test.repository.SmsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
